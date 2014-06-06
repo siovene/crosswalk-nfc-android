db4cf2d82211e562012f67f312dfa55d872ac7d0
Salvatore Iovene <salvatore.iovene@intel.com>
2014-06-06 22:16:39 +0300

    Remove the alert() and use some regular text.

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 (function() {
     var sayIt = function() {
-        value = document.forms[0]["text"].value
+        var text = document.getElementById("text").value
+        var response = echo.echo(text);
 
-        alert(echo.echo(value));
+        var $response = document.getElementById("response");
+        $response.style.display = "block";
+
+        var $you = $response.getElementsByClassName("you")[0];
+        var $crosswalk = $response.getElementsByClassName("crosswalk")[0];
+
+        $you.textContent = text;
+        $crosswalk.textContent = response;
     }
 
     window.sayIt = sayIt;
