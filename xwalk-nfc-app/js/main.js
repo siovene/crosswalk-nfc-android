e76a07497695448988833acf91be1e140d6f03e7
Salvatore Iovene <salvatore@iovene.com>
2014-10-16 11:35:37 +0300

    Improve example app UI.

--- xwalk-nfc-app/js/main.js
+++ xwalk-nfc-app/js/main.js
@@ -16,28 +16,30 @@ nfc.NFCManager.prototype.onpoweroff = function () {
 nfc.NFCManager.prototype.ontagfound = function (e) {
     var tag = e.tag;
     tag.readNDEF().then(function (record) {
-        record.getPayload().then(function (payload) {
-            /*global alert */
-            alert("Record TNF: " + nfc.TNF[record.tnf] + "\n" +
-                  "Record type: " + record.type + "\n" +
-                  "Record id: " + record.id + "\n" +
-                  "Record payload: " + String.fromCharCode.apply(null, payload));
+        document.getElementById("tnf").textContent = nfc.TNF[record.tnf];
+        document.getElementById("type").textContent = record.type;
 
-            switch (record.tnf) {
-            case 0:
-                console.log("Empty record");
-                break;
+        switch (record.tnf) {
+        case 0:
+            console.log("Empty record");
+            break;
 
-            case 1:
-                if (record.type === 'T') {
-                    console.log("Record text: " + record.text);
-                    console.log("Record language code: " + record.languageCode);
-                    console.log("Record encoding: " + record.enconding);
-                } else if (record.type === 'U') {
-                    console.log("Record URI: " + record.uri);
-                }
-                break;
+        case 1:
+            if (record.type === 'T') {
+                document.getElementById('type-text').className = 'show';
+                document.getElementById('text').textContent = record.text;
+                document.getElementById('language-code').textContent = record.languageCode;
+                document.getElementById('encoding').textContent = record.encoding;
+            } else if (record.type === 'U') {
+                document.getElementById('type-uri').className = 'show';
+                document.getElementById('uri').textContent = record.uri;
             }
+            break;
+        }
+
+        record.getPayload().then(function (payload) {
+            document.getElementById("payload").textContent =
+                String.fromCharCode.apply(null, payload);
         });
     });
 };
